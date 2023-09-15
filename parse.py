0c4290ecde148dc31bd1b3f3a873fa0becefd957
Vaibhav Vanage <65243646+Phantom-IN@users.noreply.github.com>
2023-09-15 18:01:29 +0530

    Update parse.py

--- parse.py
+++ parse.py
@@ -1,9 +1,6 @@
 import re
 
-# Define a regular expression pattern to extract URLs
 url_pattern = r'Href Attribute:\s*(https?://\S+)'
-
-# Function to extract URLs from a line of text
 def extract_url(line):
     match = re.search(url_pattern, line)
     if match:
@@ -11,19 +8,13 @@ def extract_url(line):
     else:
         return None
 
-# Input file name
 input_file = 'hrefs.txt'
-# Output file name
 output_file = 'output.txt'
 
-# Open the input and output files
 with open(input_file, 'r') as infile, open(output_file, 'w') as outfile:
-    # Iterate through each line in the input file
     for line in infile:
-        # Extract the URL from the line
         url = extract_url(line)
         if url:
-            # Write the URL to the output file
             outfile.write(url + '\n')
 
 print("URLs extracted and saved in", output_file)
